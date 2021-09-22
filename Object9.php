d87fa52a27f8edbe9e06161483c4873f95a3d24b
hey-nastey <90441107+hey-nastey@users.noreply.github.com>
2021-09-23 00:08:04 +0400

    Update Object9.php

--- Object9.php
+++ Object9.php
@@ -2,15 +2,9 @@
 //посудомойка
 class dishwasher{
     //шнур питания, отсек,дверь, кнопка (старт), шланг подачи воды
-    public $power_cord, $compartment, $door, $button,$water_supply;
-
-    public function __construct($power_cord, $compartment, $door, $button,$water_supply){
-        $this->compartment = $compartment;
-        $this->door = $door;
-        $this->power_cord = $power_cord;
-        $this->water_supply = $water_supply;
-        $this->button = $button;
-    }
+    //public $power_cord, $compartment, $door, $button,$water_supply;
+    //извините но я усталь поэтому тут будет только кнопка
+    private  $button;
 
     public function washDishes(){
         /**
@@ -19,6 +13,20 @@ class dishwasher{
          * нужна только продуктивная мойка посуды
          */
     }
+    public function __construct( $button)
+    {
+        /**$this->compartment = $compartment;
+         * $this->door = $door;
+         * $this->power_cord = $power_cord;
+         * $this->water_supply = $water_supply;*/
+        $this->button = $button;
+    }
+
+    public function getButton()
+    {
+        return $this->button;
+    }
 }
-$myDishwasher = new dishwasher();
+$myDishwasher = new dishwasher('имеется и на том спасибо');
+echo 'Кнопка старта: '.$myDishwasher->getButton().'<br>';
 $myDishwasher->washDishes();
