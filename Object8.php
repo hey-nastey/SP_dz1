0dc1996c6b8f5e58716613b2086b90628c856c18
hey-nastey <90441107+hey-nastey@users.noreply.github.com>
2021-09-22 23:25:03 +0400

    Update Object8.php

--- Object8.php
+++ Object8.php
@@ -1,20 +1,33 @@
 <?php
 //Акустическая система
 class SpeakerSystem{
-    //кол-во колонок, кол-во полос, материал
-    public $columns, $bands, $material;
+    //свойства
+    public $columns, $material;//кол-во колонок, материал
 
-    public function __construct($columns, $bands, $material){
-        $this->material = $material;
-        $this->columns = $columns;
-        $this->bands = $bands;
-    }
-
+    //метод класса
     public function playback(){
         /**
          * реализация воспроизведения мелодии
          */
     }
+    //конструктор
+    public function __construct($columns, $material){
+        $this->material = $material;
+        $this->columns = $columns;
+    }
+
+    //геттеры
+    public function getMaterial()
+    {
+        return $this->material;
+    }
+    public function getColumns()
+    {
+        return $this->columns;
+    }
+
 }
-$mySpeakerSystem = new SpeakerSystem(2,3,'Дерево');
+$mySpeakerSystem = new SpeakerSystem(2,'Дерево');
+echo 'Кол-во колонок: '.$mySpeakerSystem->getColumns().'<br>';
+echo 'Материал: '.$mySpeakerSystem->getMaterial().'<br>';
 $mySpeakerSystem->playback();
