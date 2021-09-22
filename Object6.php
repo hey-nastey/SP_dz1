17f0b56417b1f1083229a54f3f07d114443c3693
hey-nastey <90441107+hey-nastey@users.noreply.github.com>
2021-09-22 23:24:15 +0400

    Update Object6.php

--- Object6.php
+++ Object6.php
@@ -1,20 +1,38 @@
 <?php
 //электрический чайник
 class electricKettle{
-    //материал,подсветка, кнопка(вкл/выкл)
-    public $material,$illumination, $button;
+    //свойства
+    private $material,$illumination, $button;//материал,подсветка, кнопка(вкл/выкл)
 
-    public function __construct($material,$illumination, $button){
-        $this->illumination = $illumination;
-        $this->material = $material;
-        $this->button = $button;
-    }
-
+    //метод класса
     public function boil(){
         /**
          * кипятим водичку
          */
     }
+
+    //конструктор
+    public function __construct($material,$illumination, $button){
+    $this->illumination = $illumination;
+    $this->material = $material;
+    $this->button = $button;
+    }
+
+    //геттеры
+    public function getButton()
+    {
+        return $this->button;
+    }
+    public function getIllumination()
+    {
+        return $this->illumination;
+    }
+    public function getMaterial()
+    {
+        return $this->material;
+    }
+
 }
-$myElectricKettle = new electricKettle('Стекло','розовенький','ну кнопка есть');
+$myElectricKettle = new electricKettle('стекла','розовенькой','работающей');
+echo 'Электрический чайник из '.$myElectricKettle->getMaterial().' c '.$myElectricKettle->getIllumination().' подсветкой и '.$myElectricKettle->getButton().' кнопкой';
 $myElectricKettle->boil();
