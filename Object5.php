476581cf724a0f09b4ff1993f2e7a4e59e3397e0
hey-nastey <90441107+hey-nastey@users.noreply.github.com>
2021-09-22 23:23:48 +0400

    Update Object5.php

--- Object5.php
+++ Object5.php
@@ -1,20 +1,39 @@
 <?php
 //Колесо обозрения
 class ferrisWheel{
-    //Сиденья,Двери,Окна
-    public $seats,$doors,$windows;
+    //свойства
+    private $seats,$doors,$windows;//Сиденья,Двери,Окна
 
-    public function __construct($seats,$doors,$windows){
-        $this->doors = $doors;
-        $this->seats = $seats;
-        $this->windows = $windows;
-    }
-
+    //метод класса
     public function rotation(){
         /**
          *реализация вращения
          */
     }
+
+    //конструктор
+    public function __construct($seats = 'мягкие',$doors = 'надежные',$windows= 'имеются'){
+        $this->doors = $doors;
+        $this->seats = $seats;
+        $this->windows = $windows;
+    }
+
+    //геттеры
+    public function getDoors()
+    {
+        return $this->doors;
+    }
+    public function getSeats()
+    {
+        return $this->seats;
+    }
+    public function getWindows()
+    {
+        return $this->windows;
+    }
 }
 $myFerrisWheel = new ferrisWheel();
+echo 'Сиденья: '.$myFerrisWheel->getSeats().'<br>';
+echo 'Двери: '.$myFerrisWheel->getDoors().'<br>';
+echo 'Окна: '.$myFerrisWheel->getWindows().'<br>';
 $myFerrisWheel ->rotation();
